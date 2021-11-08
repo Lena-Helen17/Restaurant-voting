3b84ca9c4ac198a9eceeeeb4d8260b670e2a82f9
Мошкова Елена Анатольевна <moshkova_ea@nartis.ru>
2021-11-08 10:41:43 +0300

    Created spring boot 2.x HATEOAS

---
+++ src/main/java/com/lenaHelen17/project/RestaurantVotingApplication.java
@@ -0,0 +1,13 @@
+package com.lenaHelen17.project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestaurantVotingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestaurantVotingApplication.class, args);
+    }
+
+}
