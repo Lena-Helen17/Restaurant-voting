12bb065100746047ffdcb31fb0ecb0a201ad4ae6
Мошкова Елена Анатольевна <moshkova_ea@nartis.ru>
2021-12-01 09:29:10 +0300

    Add Spring Data Rest

--- src/main/java/com/lenaHelen17/project/repository/UserRepository.java
+++ src/main/java/com/lenaHelen17/project/repository/UserRepository.java
@@ -3,6 +3,7 @@ package com.lenaHelen17.project.repository;
 import com.lenaHelen17.project.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.transaction.annotation.Transactional;
 
 
@@ -12,8 +13,10 @@ import java.util.Optional;
 @Transactional(readOnly = true)
 public interface UserRepository extends JpaRepository<User, Integer> {
 
-//    @Query("SELECT u FROM User u WHERE u.email = LOWER(:email)")
-//    Optional<User> findByEmailIgnoreCase(String email);
+    @RestResource(rel = "by-email", path = "by-email")
+    @Query("SELECT u FROM User u WHERE u.email = LOWER(:email)")
+    Optional<User> findByEmailIgnoreCase(String email);
 
+    @RestResource(rel = "by-lastname", path = "by-lastname")
     List<User> findByLastNameContainingIgnoreCase(String lastName);
 }
