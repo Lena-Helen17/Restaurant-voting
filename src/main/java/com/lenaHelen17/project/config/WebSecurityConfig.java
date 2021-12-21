ef7065264c111c51a21de2fb73a78598f89dcaa7
Мошкова Елена Анатольевна <moshkova_ea@nartis.ru>
2021-12-21 16:42:36 +0300

    Add RestController

--- src/main/java/com/lenaHelen17/project/config/WebSecurityConfig.java
+++ src/main/java/com/lenaHelen17/project/config/WebSecurityConfig.java
@@ -49,6 +49,7 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity httpSecurity) throws Exception {
         httpSecurity.authorizeRequests()
+                .antMatchers("/api/account/register").anonymous()
                 .antMatchers("/api/account").hasRole(Role.USER.name())
                 .antMatchers("/api/**").hasRole(Role.ADMIN.name())
                 .and().httpBasic()
